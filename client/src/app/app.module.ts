945dab80903c95c55c725cebfcd3a75d72548abc
Ilia Isaev <Ilia_Isaev@epam.com>
2021-02-27 17:03:12 +0400

    Created main page component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './PL/login-page/login-page.component';
+import { ToDoMainPageComponent } from './PL/to-do-main-page/to-do-main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    ToDoMainPageComponent
   ],
   imports: [
     BrowserModule,
